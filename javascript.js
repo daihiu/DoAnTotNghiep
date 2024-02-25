df84d9a489faf56819ea9281de317592cbcc9e69
Nguyễn Hữu Đại <dainh@gemvietnam.com>
2024-02-25 23:23:23 +0700

    b

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+
 // Du lịch
 // với bạn là gì?
 // Là kỷ niệm, là trải nghiệm, là kiến thức..
@@ -47,12 +48,30 @@ setTimeout(function () {
     document.getElementById("startButton").style.transition = "opacity 1s";
 }, 10500);
 
-function startShowOtherScreen(){
+function startShowOtherScreen() {
     //hide the main page
+    console.log("start");
+    //set session storage to store the data
+    sessionStorage.setItem("data", "0");
+    //data will expire after 24h
+    setTimeout(function () {
+        sessionStorage.clear();
+    }, 86400000);
+    console.log(sessionStorage.getItem("data"));
+
     document.getElementById("firstPage").style.display = "none";
     //show the other screen
     document.getElementById("wrapper").style.display = "flex";
     //add music for main page
     var audio = new Audio('./sounds/dulich.mp3');
     audio.play();
+}
+
+function setSessionStorage(x) {
+    var session = sessionStorage.getItem("data");
+    if (session == "0") {
+        sessionStorage.setItem("data", x);
+    } else {
+        sessionStorage.setItem("data", "0" + x.toString());
+    }
 }
